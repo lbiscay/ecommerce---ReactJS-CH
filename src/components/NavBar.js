c1131b548821bfb9e33216a34418daa6cc9de00f
lbiscay <lucas.biscay@gmail.com>
2022-03-01 19:06:48 -0300

    Clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <nav>
+    <h1>Libreria virtual</h1>
+    <a href="#">Inicio</a>
+    <a href="#">Catálogo</a>
+    <a href="#">Carrito</a>
+</nav>
+  )
+}
+
+export default NavBar
